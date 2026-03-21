b3c8e23c706b9bdf5046b2a8e13b81f11cef58c1
vicky <pratamavr@gmail.com>
2026-03-21 18:54:50 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Modules\QuranAndHadits\Http\Controllers\QuranController;
 
-Route::prefix('apps')->name('apps.')->group(function () {
+Route::prefix('apps')
+->name('apps.')
+->middleware('telegram.miniapp')
+->group(function () {
   Route::prefix('quran')->name('quran.')->group(function () {
     Route::get('/', [QuranController::class, 'index'])->name('index');
     Route::get('/surah/{surah:number}', [QuranController::class, 'show'])->name('surah');
