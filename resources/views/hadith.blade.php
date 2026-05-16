1302caac7637afa17fee4f74283b0224ec53c473
vicky <pratamavr@gmail.com>
2026-05-17 03:40:35 +0800

    updates

--- resources/views/hadith.blade.php
+++ resources/views/hadith.blade.php
@@ -51,7 +51,6 @@
   }
   .book-item:active, .hadith-item:active {
     background-color: var(--tg-theme-section-separator-color);
-    transform: scale(0.28);
   }
   mark {
     background-color: #ffeb3b;
